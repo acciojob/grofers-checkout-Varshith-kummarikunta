e065a05000768fdf12ea5cdba6fc1c1cd9412d0c
Varshith-kummarikunta <varshithkummarikunta@gmail.com>
2025-09-16 19:31:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,17 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	 const prices = document.querySelectorAll('.price');
+  let sum = 0;
+  prices.forEach(p => sum += parseFloat(p.textContent));
+
+  const row = document.createElement('tr');
+  const cell = document.createElement('td');
+  cell.colSpan = 2;
+  cell.textContent = `Total Price: ₹${sum}`;
+  row.appendChild(cell);
+  document.querySelector('table').appendChild(row);
+	
 };
 
 getSumBtn.addEventListener("click", getSum);
